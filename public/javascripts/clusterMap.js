518681b6cf17e03f17246d44e1702c323cef35c5
MarkoCuk54 <marko.cuk.888@gmail.com>
2021-04-14 17:29:26 +0200

    fix map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
-    zoom: 3
+    center: [55.4833314, 28.7999968],
+    zoom: 2
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -129,4 +129,3 @@ map.on('load', function () {
         map.getCanvas().style.cursor = '';
     });
 });
-
